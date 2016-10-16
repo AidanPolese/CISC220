b50741e293916f12b7551cb1314517a9880a1ff3
alive <alive@Alive.local>
2016-10-16 16:57:17 -0400

    Tested on two machines - should work

---
+++ ipExtractor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+var=$(ifconfig | awk 'sub(/inet /,""){print $1}')
+	
+	#read var and grep for IPs that match and assign Local/Private to them
+	echo "$var" | grep -e "127" -e "10" -e "192.168" | while read -r line ; do
+                if [[ "$line" =~ ^127* ]]; then
+                        echo "Local IP: $line"
+                elif [[ "$line" =~ ^192.168.* || "$line" =~ ^10* ]];then
+            		echo "Private IP: $line"           
+		fi
+        done
+	
+	#read var and grep for IPs that do not match - loop and assign public IP to them
+	echo "$var" | grep -e "127" -e "10" -e "192.168" -v | while read -r line ; do
+                echo "Public IP: $line"
+        done
+	
